7e0b41ee2b41294706cba4949543ce38bda6ac00
duc24600 <duc24600@gmail.com>
2021-11-03 01:35:18 +0700

    Duc Doan Commit LiveScore

---
+++ app/src/main/java/com/example/demolivescore/service/MatchAPI.java
@@ -0,0 +1,13 @@
+package com.example.demolivescore.service;
+
+import com.example.demolivescore.dto.LiveScoreDto;
+import com.example.demolivescore.dto.MatchDetailDto;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MatchAPI {
+    @GET("https://livescore-api.com/api-client/matches/stats.json?&key=pyz5S7m6r0SC5Wrn&secret=qopVnOKW2wfSzyxY664Zer5GsXixTKVk")
+    Call<MatchDetailDto> getMatchStatistic(@Query("match_id") String matchId);
+}
---
+++ app/src/main/java/com/example/demolivescore/ui/main/PlaceholderFragment.java
@@ -0,0 +1,70 @@
+package com.example.demolivescore.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.demolivescore.R;
+import com.example.demolivescore.databinding.FragmentDetailMatchBinding;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceholderFragment extends Fragment {
+    public static int[] resourceIds = {R.layout.fragment_statistic, R.layout.fragment_detail_match};
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private PageViewModel pageViewModel;
+    private FragmentDetailMatchBinding binding;
+
+    public static PlaceholderFragment newInstance(int index) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageViewModel = new ViewModelProvider(this).get(PageViewModel.class);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        pageViewModel.setIndex(index);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+
+        binding = FragmentDetailMatchBinding.inflate(inflater, container, false);
+        int index = getArguments().getInt(ARG_SECTION_NUMBER);
+        View rootView = inflater.inflate(resourceIds[index], container, false);
+//        pageViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        return rootView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
